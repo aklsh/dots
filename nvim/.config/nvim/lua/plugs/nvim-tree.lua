b4762f24478710742e340792817da08bb349c015
Akilesh Kannan <aklsh@tuta.io>
2023-04-13 15:04:12 +0530

    remove deprecated options

--- nvim/.config/nvim/lua/plugs/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugs/nvim-tree.lua
@@ -11,8 +11,6 @@ require("nvim-tree").setup({
   disable_netrw = false,
   respect_buf_cwd = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = false,
